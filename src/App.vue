304868b20b1a715fb6ae134129b45b694fa4ccdc
julien REYNAUD <julienjulienreynaud@outlook.fr>
2024-02-20 14:21:06 +0000

    creation des différents .vue

--- src/App.vue
+++ src/App.vue
@@ -14,17 +14,22 @@ import HelloWorld from './components/HelloWorld.vue'
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+
+<template>
+  <div>
+    <router-link to="/">Accueil</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
+
+<template>
+
+
+
+</template>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import router from './router'; // Importez le fichier de configuration du routeur
+
+createApp(App).use(router).mount('#app');
+
 createApp(App).mount('#app')
